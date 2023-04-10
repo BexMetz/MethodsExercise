16b19ddde6df475b112a6af8beb3f99ca14b8814
Bex <bexmetzger@gmail.com>
2023-04-10 16:37:00 -0700

    added user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hey there! Can I get your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Well hello {userName}! Tell me, what color is your favorite?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is one of my favorites! And what about your favorite animal, {userName}?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Woah! A {color} {animal} sounds wild! One last question {userName}; What band is your favorite band?");
+            var band = Console.ReadLine();
+
         }
     }
 }
