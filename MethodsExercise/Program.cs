8bcb8e43d54460013dc992e6b770e0b5fa1fe476
Bex <bexmetzger@gmail.com>
2023-04-11 16:52:46 -0700

    exercise two

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,9 +1,41 @@
 ﻿namespace MethodsExercise
 {
-    public class Program
+    class Program
     {
+        public static int Sum(int num1, int num2)
+        { 
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Add(int num1, int num2, int num3, int num4, int num5)
+        {
+            return num1 + num2 + num3 + num4 + num5;
+        }
+
         static void Main(string[] args)
         {
+
+            var amountOfCars = Sum(2, 6);
+            var something = Multiply(60, 2, 4);
+            var blah = Divide(720, 12);
+            var result = Subtract(70, 33);
+            var whatever = Add(7, 4, 21, 87, 90);
+
             Console.WriteLine("Hey there! Can I get your first name?");
             var userName = Console.ReadLine();
 
